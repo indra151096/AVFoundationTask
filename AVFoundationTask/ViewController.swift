4b5204d498acce10f2912e0162186b803cb8db3f
Indra <indra.sumawi@gmail.com>
2019-07-10 12:36:55 +0700

    Initial Commit

---
+++ AVFoundationTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundationTask
+//
+//  Created by Indra Sumawi on 10/07/19.
+//  Copyright © 2019 Indra Sumawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
